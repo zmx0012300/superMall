779b2fe40536def993b623b668327c636f3b4450
曾美星 <879209978@qq.com>
2020-06-06 18:38:20 +0800

    底部tabbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
-  <div id="app">
-    hh
+  <div id="app" class="wrapper">
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
-<style>
+<script>
+import MainTabBar from 'components/content/mainTabbar/MainTabBar'
+export default {
+  name: 'App',
+  components: {
+    MainTabBar
+  }
+}
 
+</script>
+<style>
+  @import url('assets/css/base.css');
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-    
+    configureWebpack: {
+        resolve: {
+            alias: {
+                'assets': '@/assets',//@已经配置为src
+                'common': '@/common',
+                'components': '@/components',
+                'views': '@/views',
+                'network': '@/network'
+            }
+        }
+    }
 }
